c916e0a96d4e1b7e86511d406c56ab86a71e1a97
D.A.Bashkirtsevich <bashkirtsevich@gmail.com>
2018-04-22 20:28:27 +0500

    Refactor: move TextSprite to different module

--- pytile.py
+++ pytile.py
@@ -4,6 +4,7 @@ import pygame
 
 import tools
 import world
+from text_sprite import TextSprite
 from vec2d import *
 
 World = world.World()
@@ -35,70 +36,6 @@ p16 = int(p / 16)
 
 # tile height difference
 ph = 8
-
-
-class TextSprite(pygame.sprite.Sprite):
-    """Subclass of sprite to draw text to the screen"""
-
-    def __init__(self, position, text, font, fg=(0, 0, 0), bg=None,
-                 border_width=0, border_color=(0, 0, 0),
-                 bold=False, italic=False, underline=False,
-                 line_spacing=3, padding=5):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.position = position
-        self.font = font
-        self.fg = fg
-        self.bg = bg
-        self.border_width = border_width
-        self.border_color = border_color
-        self.line_spacing = line_spacing
-        self.padding = padding
-        self.font.set_bold(bold)
-        self.font.set_italic(italic)
-        self.font.set_underline(underline)
-
-        self.rect = None
-
-        self.image = None
-
-        self.text = text
-        self.update()
-
-    def update(self):
-        """"""
-        text_images = []
-        # Render all lines of text
-        for t in self.text:
-            text_images.append(self.font.render(t, False, self.fg, self.bg))
-
-        # Find the largest width line of text
-        max_width = max(text_images, key=lambda x: x.get_width()).get_width()
-        # Produce an image to hold all of the text strings
-        self.image = pygame.Surface(
-            (max_width + 2 * (self.border_width + self.padding),
-             text_images[0].get_height() * len(text_images) + self.line_spacing * (len(text_images) - 1) + 2 * (
-                     self.border_width + self.padding)
-             )
-        )
-        self.image.fill(self.bg)
-
-        if self.border_width > 0:
-            pygame.draw.rect(self.image, self.border_color,
-                             (0, 0, self.image.get_width(), self.image.get_height()), self.border_width)
-
-        for n, t in enumerate(text_images):
-            self.image.blit(t, (self.border_width + self.padding,
-                                self.border_width + self.padding + (self.line_spacing + t.get_height()) * n))
-
-        # Store the last rect so if the new one is smaller we can update those bits of the screen too
-        last_rect = self.rect
-        self.rect = pygame.Rect(self.position[0], self.position[1], self.image.get_width(), self.image.get_height())
-
-        if last_rect is None:
-            return self.rect
-        else:
-            return last_rect.union(self.rect)
 
 
 class TileSprite(pygame.sprite.Sprite):
@@ -327,12 +264,12 @@ class DisplayMain(object):
                         if event.key == pygame.K_t:
                             # Activate track drawing mode
                             self.lmb_tool = tools.Track()
-                            self.active_tool_sprite.text = ["Track drawing"]
+                            self.active_tool_sprite.text_lines = ["Track drawing"]
                             self.dirty.append(self.active_tool_sprite.update())
                         if event.key == pygame.K_h:
                             # Activate terrain modification mode
                             self.lmb_tool = tools.Terrain()
-                            self.active_tool_sprite.text = ["Terrain modification"]
+                            self.active_tool_sprite.text_lines = ["Terrain modification"]
                             self.dirty.append(self.active_tool_sprite.update())
 
                         # Some tools may use the escape key
