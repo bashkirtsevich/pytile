ab3669ee42dab535702b51eee87d388f62319d39
D.A.Bashkirtsevich <bashkirtsevich@gmail.com>
2018-04-25 23:38:49 +0500

    PEP8 fixes

--- tools.py
+++ tools.py
@@ -409,26 +409,26 @@ class Terrain(Tool):
                 maxval = max(vertices, key=lambda x: x[0])[0]
                 if maxval != 0:
                     rr = 0
-                    for p in vertices:
-                        if p[0] == maxval:
-                            p[0] -= 1
+                    for point in vertices:
+                        if point[0] == maxval:
+                            point[0] -= 1
                             # Whole tile lower
                             if subtile == 9:
-                                tgrid = World.get_height(p[1])
+                                tgrid = World.get_height(point[1])
                                 rr = tgrid.lower_face()
-                                World.set_height(tgrid, p[1])
+                                World.set_height(tgrid, point[1])
                             # Edge lower
                             elif subtile in [5, 6, 7, 8]:
                                 st1 = subtile - 5
                                 st2 = st1 + 1
-                                tgrid = World.get_height(p[1])
+                                tgrid = World.get_height(point[1])
                                 rr = tgrid.lower_edge(st1, st2)
-                                World.set_height(tgrid, p[1])
+                                World.set_height(tgrid, point[1])
                             # Vertex lower
                             elif subtile in [1, 2, 3, 4]:
-                                tgrid = World.get_height(p[1])
+                                tgrid = World.get_height(point[1])
                                 rr = tgrid.lower_vertex(subtile - 1)
-                                World.set_height(tgrid, p[1])
+                                World.set_height(tgrid, point[1])
                     # Since we're potentially modifying a large number of individual tiles we only want to know if
                     # *any* of them were lowered for the purposes of calculating the real raise/lower amount
                     # Thus r should only be incremented once per raise/lower level
@@ -448,27 +448,27 @@ class Terrain(Tool):
             step = 1
             for i in range(0, amount, step):
                 # TODO: Fix it when "vertices" is empty
-                minval = min(vertices, key=lambda x: x[0])[0]
-                for p in vertices:
-                    if p[0] == minval:
-                        p[0] += 1
+                min_val = min(vertices, key=lambda x: x[0])[0]
+                for point in vertices:
+                    if point[0] == min_val:
+                        point[0] += 1
                         # Whole tile raise
                         if subtile == 9:
-                            tgrid = World.get_height(p[1])
+                            tgrid = World.get_height(point[1])
                             tgrid.raise_face()
-                            World.set_height(tgrid, p[1])
+                            World.set_height(tgrid, point[1])
                         # Edge raise
                         elif subtile in [5, 6, 7, 8]:
                             st1 = subtile - 5
                             st2 = st1 + 1
-                            tgrid = World.get_height(p[1])
+                            tgrid = World.get_height(point[1])
                             tgrid.raise_edge(st1, st2)
-                            World.set_height(tgrid, p[1])
+                            World.set_height(tgrid, point[1])
                         # Vertex raise
                         elif subtile in [1, 2, 3, 4]:
-                            tgrid = World.get_height(p[1])
+                            tgrid = World.get_height(point[1])
                             tgrid.raise_vertex(subtile - 1)
-                            World.set_height(tgrid, p[1])
+                            World.set_height(tgrid, point[1])
             if soft:
                 # Soften around the modified tiles
                 self.soften(self.aoe, soften_up=True)
